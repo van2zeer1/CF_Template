a928e84c08ac0f16ab70b1f6a7b9c5cbab4b873c
van2zeer1 <van2zeer@gmail.com>
2021-01-29 22:12:54 +0300

    Upload files

---
+++ Hello.cpp
@@ -0,0 +1,50 @@
+/*
+
+\`\   /`/```\     ncknm:   Ivan 'van2zeer' Ilyakov
+ \ \_/ / === \    cf:      https://codeforces.com/profile/karginux
+  \   / ===== \   gh:      https://github.com/van2zeer1
+   \_/_/     \_\  vk:      https://vk.com/gnu_go
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// def //  short  // var //
+ 
+#define     i64     long long
+#define     i32     int
+#define     i16     short
+#define     i8      char
+#define     u64     unsigned long long
+#define     u32     unsigned
+#define     u16     unsigned short
+#define     u8      unsigned char
+ 
+#define     vec     vector
+#define     vi      vec<i5>
+#define     vl      vec<i6>
+#define     vc      vec<i3>
+ 
+#define     elif    else if
+#define     w(i)    while((i)--)
+#define     f(i, n)   for(i32 (i) = 0; (i) < (n); (i)++)
+/* ================================== */  
+
+namespace go
+{
+	template<typename T>
+	T gcd(T a, T b) { return b == 0 ? a : go::gcd(b, a % b); }
+}
+
+i32 main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    #ifdef _DEBUG 
+	    freopen("io/i.in", "r", stdin);
+	    freopen("io/o.out", "w", stdout);
+    #endif
+    cout << "Hello world";
+}
+
